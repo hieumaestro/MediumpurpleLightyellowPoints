a1dcf75533023d964b2ab01b783dbe526e71333e
hieumaestro <54903623+hieumaestro@users.noreply.github.com>
2019-10-06 16:38:00 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+var a = [1, 2];
+var b = [1, 2];
+console.log(a === b);
+// Chạy chương trình và tìm hiểu vì sao kết quả lại là false mặc dù giá trị của 2 array có vẻ giống nhau. Viết câu trả lời ở dưới đây.
+// Do phép so sánh === so sánh địa chỉ lưu trữ của 2 array chứ không so sánh giá trị lưu trữ của chúng.
